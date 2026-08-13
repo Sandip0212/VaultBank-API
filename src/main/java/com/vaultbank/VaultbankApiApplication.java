8c06be50c9d9c8e47cfde2fae342d1598dfc5fe8
Sandip Gavandhare <sandipgavandhare3.com>
2026-08-13 17:17:24 +0530

    VaultBank API project setup with Spring Boot 4

---
+++ src/main/java/com/vaultbank/VaultbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.vaultbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultbankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaultbank/VaultbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
